7b286d4edf08d2a4e7bec6d1936357b84d9edf97
jborowiecki <166690570+jborowiecki@users.noreply.github.com>
2024-06-06 11:22:56 +0200

    postgres

---
+++ notatki.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+
+db_params=psycopg2.connect(
+    database="postgres",
+    user="postgres",
+    password='postgres',
+    host="localhost",
+    port="5432"
+)
+
+def add_new_to_table(db_params) -> None:
+    imie = input('Imie: ')
+    nazwisko = input('Nazwisko: ')
+    post = input('Post: ')
+    miejscowosc = input('Miejscowosc: ')
+
+    sql_add_query=f"INSERT INTO public.users(name, surname, post, location, cords)VALUES ('{imie}', '{nazwisko}', '{post}', '{miejscowosc}', 'SRID=4326;POINT(21.0 52.23)');"
+    cursor=db_params.cursor()
+    cursor.execute(sql_add_query)
+    db_params.commit()
+
+# add_new_to_table(db_params)
+
+
+
+def show_users(db_params) -> None:
+    sql_add_query=f"SELECT * FROM public.users"
+    cursor=db_params.cursor()
+    cursor.execute(sql_add_query)
+    users=cursor.fetchall()
+    # print(users)
+    # db_params.commit()
+    for user in users:
+        print(user)
+show_users(db_params)
